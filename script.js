eb2646fbd571b18fb2efe0311f0679240c46169a
GanikaSharma <itsganikasharma16@gmail.com>
2025-05-23 11:26:49 +0530

    Fixed the issue with restart button position and winner text message display

--- script.js
+++ script.js
@@ -98,6 +98,7 @@ function handleCellClick(event) {
       winningCell.classList.add("winning-cell");
     });
     document.getElementById("message").textContent = `Player ${currentPlayer} Wins! 🎉`;
+    document.getElementById("message").style.color = currentPlayer === 1 ? "#00ffcc" : "#ff99cc";
     gameActive = false;
     document.getElementById("restart-btn").style.display = 'block';
     return;
